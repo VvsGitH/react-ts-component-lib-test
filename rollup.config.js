39dff5b2ccd445437919904fa43c22f1890b20df
Vito Paparella Santorsola <vs.paparella@cybsec.it>
2023-10-31 16:50:09 +0100

    Feat: included boostrap-italia as dep

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,13 @@
-const resolve = require("@rollup/plugin-node-resolve");
-const typescript = require("@rollup/plugin-typescript");
-const terser = require("@rollup/plugin-terser");
 const json = require("@rollup/plugin-json");
+const resolve = require("@rollup/plugin-node-resolve");
+const terser = require("@rollup/plugin-terser");
+const typescript = require("@rollup/plugin-typescript");
 const external = require("rollup-plugin-peer-deps-external");
 const postcss = require("rollup-plugin-postcss");
 const sourcemaps = require('rollup-plugin-sourcemaps');
 
 const pkg = require("./package.json");
+const image = require('@rollup/plugin-image');
 
 module.exports = [
   {
@@ -14,29 +15,30 @@ module.exports = [
     output: [
       {
         file: pkg.main,
-        format: 'cjs',
+        format: "cjs",
         sourcemap: true,
-        exports: 'named',
+        exports: "named"
       },
       {
         file: pkg.module,
-        format: 'esm',
+        format: "esm",
         sourcemap: true,
-        exports: 'named',
-      },
+        exports: "named"
+      }
     ],
     plugins: [
       external(),
       postcss(),
+      image(),
       json(),
-      typescript({ tsconfig: './tsconfig.json' }),
+      typescript({ tsconfig: "./tsconfig.json" }),
       resolve(),
       sourcemaps(),
       terser({
         output: { comments: false },
         compress: { drop_console: true }
-      }),
+      })
     ],
-    external: ['react', 'react-dom'],
-  },
+    external: ["react", "react-dom", "bootstrap-italia"]
+  }
 ];
