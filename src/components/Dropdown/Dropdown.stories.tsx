8921e0a9aec752abad9e2428fd2eac6380c1dc5f
MarcoMolinari <themolinario@gmail.com>
2023-11-13 12:23:47 +0100

    Feature: dropdown stories

--- src/components/Dropdown/Dropdown.stories.tsx
+++ src/components/Dropdown/Dropdown.stories.tsx
@@ -19,8 +19,47 @@ type Story = StoryObj<typeof meta>;
 export const CompleteDropdown = {
   render: () => (
     <Dropdown.Container>
-      <Dropdown.Button variant="primary">Ciao</Dropdown.Button>
+      <Dropdown.Button variant="primary">Dropdown</Dropdown.Button>
       <Dropdown.Menu placement="bottom">
+        <Dropdown.Item>Primo</Dropdown.Item>
+        <Dropdown.Item>Secondo</Dropdown.Item>
+        <Dropdown.Item>Terzo</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown.Container>
+  )
+};
+
+export const DropdownTop = {
+  render: () => (
+    <Dropdown.Container>
+      <Dropdown.Button variant="primary">Dropdown</Dropdown.Button>
+      <Dropdown.Menu placement="top">
+        <Dropdown.Item>Primo</Dropdown.Item>
+        <Dropdown.Item>Secondo</Dropdown.Item>
+        <Dropdown.Item>Terzo</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown.Container>
+  )
+};
+
+export const DropdownRight = {
+  render: () => (
+    <Dropdown.Container>
+      <Dropdown.Button variant="primary">Dropdown</Dropdown.Button>
+      <Dropdown.Menu placement="right">
+        <Dropdown.Item>Primo</Dropdown.Item>
+        <Dropdown.Item>Secondo</Dropdown.Item>
+        <Dropdown.Item>Terzo</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown.Container>
+  )
+};
+
+export const DropdownLeft = {
+  render: () => (
+    <Dropdown.Container>
+      <Dropdown.Button variant="primary">Dropdown</Dropdown.Button>
+      <Dropdown.Menu placement="left">
         <Dropdown.Item>Primo</Dropdown.Item>
         <Dropdown.Item>Secondo</Dropdown.Item>
         <Dropdown.Item>Terzo</Dropdown.Item>
